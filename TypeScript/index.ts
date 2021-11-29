f4d4958ba84c2480ebe6c08a025d3d5f992cdb79
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-11-29 14:14:48 +0100

    Actually initialising the events and exporting the client

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -15,18 +15,23 @@ client.on("ready", () => {
 	let commandsInitialiser = require("./initialise-commands.ts");
 	if(commandsInitialiser.default) commandsInitialiser = commandsInitialiser.default;
 
-	const commands = commandsInitialiser();
-	console.log(commands);
+	const commands: { [key: string]: any } = commandsInitialiser();
 
 	let eventsInitialiser = require("./initialise-events.ts");
 	if(eventsInitialiser.default) eventsInitialiser = eventsInitialiser.default;
 
-	const events = eventsInitialiser();
-	console.log(events);
-});
+	const events: {[key: string]: any}[] = eventsInitialiser();
 
-client.on("messageCreate", (message) => {
-	console.log(message.content);
+	const eventArgs: {[key: string]: any} = {
+		"messageCreate": commands
+	}
+	for(const event of events) {
+		event.callback(client, eventArgs[event.name]);
+	};
 });
 
 client.login(process.env.BOT_TOKEN);
+
+export default {
+	client: client
+};
