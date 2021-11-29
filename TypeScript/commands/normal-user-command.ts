33c982724c69cfe8c976c928e292ae43461fec43
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-11-29 15:10:44 +0100

    Update normal-user-command.ts

--- TypeScript/commands/normal-user-command.ts
+++ TypeScript/commands/normal-user-command.ts
@@ -3,6 +3,6 @@ import { Message } from "discord.js";
 export default {
 	commands: ["user", "user-command"],
 	callback: (message: Message, ...args: string[]) => {
-		console.log("Command ran");
+		message.reply("Command ran successfully!");
 	}
 };
