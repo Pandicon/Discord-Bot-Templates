ae6e6bdbd2f2c4ecca3cdf1502aa513086ddb9a7
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-11-29 15:10:59 +0100

    Getting rid of default values

--- TypeScript/commands/staff-commands/admin-command.ts
+++ TypeScript/commands/staff-commands/admin-command.ts
@@ -2,12 +2,9 @@ import { Message } from "discord.js";
 
 export default {
 	commands: ["admin", "admin-command"],
-	expectedArgs: "",
 	permissionError: "You need administrator permissions to run this command",
-	minArgs: 0,
-	maxArgs: null,
 	permissions: ["ADMINISTRATOR"],
 	callback: (message: Message, ...args: string[]) => {
-		console.log("Command ran");
+		message.reply("Command ran successfully!");
 	}
 };
